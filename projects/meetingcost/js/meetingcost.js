f09ec92ff4aeeda84d872e015fed9b0795c33ea6
Sean Boots <sean.boots@gmail.com>
2016-09-29 00:10:16 -0400

    Move rates data into a separate JS file

--- projects/meetingcost/js/meetingcost.js
+++ projects/meetingcost/js/meetingcost.js
@@ -2,24 +2,6 @@
 
 var app = app || {};
 
-// Placeholder for actual rates of pay data
-app.rates = [
-	{
-		'label': 'EC-01',
-		'description': 'Economics and Social Science',
-		'min': '44350',
-		'max': '56214',
-		'median': '50282',
-	},
-	{
-		'label': 'EC-02',
-		'description': 'Economics and Social Science',
-		'min': '50122',
-		'max': '62026',
-		'median': '56074',
-	},
-];
-
 $(function() {
 
 	app.meetingCost = {};
---
+++ projects/meetingcost/js/rates.js
@@ -0,0 +1,21 @@
+// JS for the Meeting Cost Calculator
+
+var app = app || {};
+
+// Placeholder for actual rates of pay data
+app.rates = [
+	{
+		'label': 'EC-01',
+		'description': 'Economics and Social Science',
+		'min': '44350',
+		'max': '56214',
+		'median': '50282',
+	},
+	{
+		'label': 'EC-02',
+		'description': 'Economics and Social Science',
+		'min': '50122',
+		'max': '62026',
+		'median': '56074',
+	},
+];
