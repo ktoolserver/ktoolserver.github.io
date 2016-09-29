45998e1ba742909d12749afc41295560b117f62a
Sean Boots <sean.boots@gmail.com>
2016-09-29 00:07:46 -0400

    Rendering and UI for the participants / chairs display

--- projects/meetingcost/js/meetingcost.js
+++ projects/meetingcost/js/meetingcost.js
@@ -24,8 +24,10 @@ $(function() {
 
 	app.meetingCost = {};
 	app.meetingCost.participants = [];
-	app.meetingCost.timeElapsedSeconds = 0;
+	app.meetingCost.timeElapsedSeconds;
 	app.meetingCost.timeElapsedMoment;
+	
+	app.meetingCost.timerIsRunning = 0;
 
 	app.meetingCost.initialize = function() {
 
@@ -33,6 +35,21 @@ $(function() {
 
 		$('.js-add-user').on('click', function(e) {
 			app.meetingCost.addUser(e);
+		});
+
+		$('.chairs-map').on('click', '.js-remove-user', function(e) {
+			app.meetingCost.removeUser(e);
+		});
+
+		$('.js-start-time').on('click', function(e) {
+			app.meetingCost.startTime(e);
+		});
+
+		$('.js-stop-time').on('click', function(e) {
+			app.meetingCost.stopTime(e);
+		});
+		$('.js-reset').on('click', function(e) {
+			app.meetingCost.reset(e);
 		});
 
 		$('.js-add-time').on('click', function(e) {
@@ -51,7 +68,44 @@ $(function() {
 			app.meetingCost.subtractTime(amount, dateType);
 		});
 
+		// Prepare the Lodash template
+		app.meetingCost.chairContainerTemplate = _.template($('#chairContainerTemplate').html());
+
+		// Initializes other re-resettable values
+		app.meetingCost.reset();
+
+		// Initial run of the timer tick:
+		_.delay(app.meetingCost.tick, 1000);
+
+	}
+
+
+	app.meetingCost.reset = function(e) {
+
+		app.meetingCost.timerIsRunning = 0;
+
+		app.meetingCost.timeElapsedSeconds = 0;
+		app.meetingCost.participants = [];
+
 		app.meetingCost.timeElapsedMoment = moment.duration(0);
+
+		app.meetingCost.updateTimeClock();
+
+		app.meetingCost.recalculateCost();
+
+		app.meetingCost.renderChairs();
+
+		// Initialize the start/stop buttons (essentially - disabling the "Stop" button while inactive)
+		app.meetingCost.stopTime();
+
+		$('.js-reset').prop('disabled', 1);
+
+
+	}
+
+	app.meetingCost.unReset = function() {
+
+		$('.js-reset').prop('disabled', 0);
 
 	}
 
@@ -67,6 +121,27 @@ $(function() {
 		app.meetingCost.participants.push(rateData);
 
 		app.meetingCost.recalculateCost();
+
+		app.meetingCost.renderChairs();
+
+		app.meetingCost.unReset();
+
+	}
+
+	app.meetingCost.removeUser = function(e) {
+
+		var elem = $(e.currentTarget);
+		var index = $(elem).attr('data-user-index');
+
+		console.log('Deleting ' + index);
+		delete app.meetingCost.participants[index];
+
+		// Clean out empty values
+		app.meetingCost.participants = _.compact(app.meetingCost.participants);
+
+		app.meetingCost.recalculateCost();
+
+		app.meetingCost.renderChairs();
 
 
 	}
@@ -85,6 +160,38 @@ $(function() {
 
 		app.meetingCost.updateTimeClock();
 
+
+	}
+
+	// Runs once per second
+	app.meetingCost.tick = function() {
+
+		// console.log('Ticking');
+
+		if(app.meetingCost.timerIsRunning) {
+			app.meetingCost.addTime(1, 'seconds');
+		}
+
+		// Run it again!
+		_.delay(app.meetingCost.tick, 1000);
+
+	}
+
+	app.meetingCost.startTime = function(e) {
+
+		app.meetingCost.timerIsRunning = 1;
+
+		$('.js-stop-time').prop('disabled', 0);
+		$('.js-start-time').prop('disabled', 1);
+
+	}
+
+	app.meetingCost.stopTime = function(e) {
+
+		app.meetingCost.timerIsRunning = 0;
+
+		$('.js-stop-time').prop('disabled', 1);
+		$('.js-start-time').prop('disabled', 0);
 
 	}
 
@@ -129,6 +236,12 @@ $(function() {
 		$('.js-cost-display').text(newCostRounded);
 
 		return newCost;
+
+	}
+
+	app.meetingCost.renderChairs = function() {
+
+		$('.chairs-map').html(app.meetingCost.chairContainerTemplate());
 
 	}
 
